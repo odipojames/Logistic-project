62393e2af261c537262a179902db214d1c09dc17
joshedewa <joshedewa@gmail.com>
2020-01-16 09:19:49 +0300

    ft-rates (#26)
    * ft-rates
    This feature handles shipping rates
    
    * ft-rates
    Among the three rate chaoices:
    The feature validates that at least one field has a value
    if none it raises an error
    
    * ft-rates
    Included feedback issued

--- rates/models.py
+++ rates/models.py
@@ -5,16 +5,16 @@ from utils.models import AbstractBaseModel, ActiveObjectsQuerySet
 
 from django.core.exceptions import ValidationError
 
-from utils.models import AbstractBaseModel, ActiveObjectsQuerySet
 from utils.helpers import enforce_all_required_arguments_are_truthy
-
+from orders.models import Order
+from decimal import Decimal
 
 class RatesManager(models.Manager):
     """
     Manager to handle methods of the rates model.
     """
 
-    def create_rates(self, type=None, prefered_currency=None):
+    def create_rates(self, type={}, prefered_currency=None):
         """
         Method to create and return Rate instances.
         """
@@ -25,7 +25,7 @@ class RatesManager(models.Manager):
             {"type": type, "prefered_currency": prefered_currency}, REQUIRED_ARGS)
 
         rates = self.model(type=type, prefered_currency=prefered_currency)
-
+        rates.clean()
         rates.save()
         return rates
 
@@ -34,6 +34,29 @@ class Rate(AbstractBaseModel, models.Model):
     type = HStoreField()
     prefered_currency = models.CharField(
         max_length=20, choices=[('USD', 'USD'), ('KES', 'KES')])
+    # Generic relations https://docs.djangoproject.com/en/2.2/ref/contrib/contenttypes/#id1
 
+    
     objects = RatesManager()
     active_objects = ActiveObjectsQuerySet.as_manager()
+
+    def clean(self):
+        # TODO validate that the charges dictionary contains necessary keys, eg cost per distance, etc
+        RATE_CHOICES = ("price_per_km", "price_per_kg", "price_per_truck")
+
+        for key in RATE_CHOICES:
+            if key in self.type.keys() is None:
+                raise ValidationError({"charges": f"{key} must be provided."})
+
+        for value in self.type.values():
+            value = Decimal(value)
+            if not isinstance(value, Decimal):
+                raise ValidationError({"Rates should be decimal value"})
+            
+            
+
+        # if a key is passed that shouldn't be included, we remove it.
+        for key in self.type.copy().keys():
+            if key not in RATE_CHOICES:
+                self.type.pop(key)
+        return super().clean()
