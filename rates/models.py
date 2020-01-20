8871e1a2fb6fdf8d8e63c5e05002765c85845448
Keith <44587521+MandelaK@users.noreply.github.com>
2020-01-20 10:55:33 +0300

    fix(Rates) Remove nested fields and make rates easier to create (#33)
    - Remove HSTORE field
    - Make all rate types primary fields
    
    [Finishes #170752253]

--- rates/models.py
+++ rates/models.py
@@ -14,50 +14,32 @@ class RatesManager(models.Manager):
     Manager to handle methods of the rates model.
     """
 
-    def create_rates(self, type={}, prefered_currency=None):
+    def create_rates(self, price_per_kg=Decimal(0), price_per_km=Decimal(0), price_per_truck=Decimal(0), preferred_currency=None, created_by=None):
         """
         Method to create and return Rate instances.
         """
 
-        REQUIRED_ARGS = ("type", "prefered_currency")
+        REQUIRED_ARGS = ("preferred_currency", "created_by")
 
         enforce_all_required_arguments_are_truthy(
-            {"type": type, "prefered_currency": prefered_currency}, REQUIRED_ARGS)
+            {"preferred_currency": preferred_currency, "created_by": created_by}, REQUIRED_ARGS)
 
-        rates = self.model(type=type, prefered_currency=prefered_currency)
-        rates.clean()
+        rates = self.model(price_per_kg=price_per_kg, price_per_km=price_per_km, price_per_truck=price_per_truck, preferred_currency=preferred_currency, created_by=created_by)
+
         rates.save()
         return rates
 
 
 class Rate(AbstractBaseModel, models.Model):
-    type = HStoreField()
-    prefered_currency = models.CharField(
+    price_per_kg = models.DecimalField(decimal_places=4, max_digits=12, blank=True, default=Decimal(0))
+    price_per_km = models.DecimalField(decimal_places=4, max_digits=12, blank=True, default=Decimal(0))
+    price_per_truck = models.DecimalField(decimal_places=4, max_digits=12, blank=True, default=Decimal(0))
+    preferred_currency = models.CharField(
         max_length=20, choices=[('USD', 'USD'), ('KES', 'KES')])
-    # Generic relations https://docs.djangoproject.com/en/2.2/ref/contrib/contenttypes/#id1
-    created_by = models.ForeignKey('companies.CargoOwnerCompany', on_delete=models.CASCADE)
+    created_by = models.ForeignKey('companies.CargoOwnerCompany', on_delete=models.CASCADE, related_name='rates')
 
-    
     objects = RatesManager()
     active_objects = ActiveObjectsQuerySet.as_manager()
 
-    def clean(self):
-        # TODO validate that the charges dictionary contains necessary keys, eg cost per distance, etc
-        RATE_CHOICES = ("price_per_km", "price_per_kg", "price_per_truck")
 
-        for key in RATE_CHOICES:
-            if key in self.type.keys() is None:
-                raise ValidationError({"charges": f"{key} must be provided."})
 
-        for value in self.type.values():
-            value = Decimal(value)
-            if not isinstance(value, Decimal):
-                raise ValidationError({"Rates should be decimal value"})
-            
-            
-
-        # if a key is passed that shouldn't be included, we remove it.
-        for key in self.type.copy().keys():
-            if key not in RATE_CHOICES:
-                self.type.pop(key)
-        return super().clean()
