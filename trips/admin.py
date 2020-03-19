b7640ad6ec740c1cbc534a84b102fbf4f268942b
odipojames <odipojames12@gmai.com>
2020-03-19 11:32:57 +0300

    [ch#171852415 ]add event model

--- trips/admin.py
+++ trips/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Trip, TripInvoice
+from .models import Trip, TripInvoice, Event
 
 # Register your models here.
 admin.site.register(Trip)
 admin.site.register(TripInvoice)
+admin.site.register(Event)
